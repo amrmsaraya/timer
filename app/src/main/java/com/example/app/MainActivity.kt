5c3679aab6e8d47627d75f94ce3fd39e1b60695a
Amr Saraya <amrmsaraya@gmail.com>
2021-12-11 18:54:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.app.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                }
+            }
+        }
+    }
+}
