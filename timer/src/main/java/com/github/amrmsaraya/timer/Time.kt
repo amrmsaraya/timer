07452cab719b687195567f6ba0b3c513671f5a5b
Amr Saraya <amrmsaraya@gmail.com>
2021-12-12 04:24:53 +0200

    Simplify time converter formula and remove days from Time

--- timer/src/main/java/com/github/amrmsaraya/timer/Time.kt
+++ timer/src/main/java/com/github/amrmsaraya/timer/Time.kt
@@ -3,7 +3,6 @@ package com.github.amrmsaraya.timer
 /**
  * Time class is a class which holds time in an easy formatted shape
  * @param timeInMillis The current timer in milliseconds
- * @param days How many days in timer
  * @param hours How many hours in timer
  * @param minutes How many minutes in timer
  * @param seconds How many seconds in timer
@@ -12,9 +11,6 @@ package com.github.amrmsaraya.timer
 data class Time(
     /** The current timer in milliseconds */
     val timeInMillis: Long = 0,
-
-    /** How many days in timer */
-    val days: Int = 0,
 
     /** How many hours in timer */
     val hours: Int = 0,
